50b7b18561c0a20cf66529e0fed136be5b501738
snake <admin@viasnake.com>
2021-07-30 23:33:36 +0900

    Rect

--- Main.py
+++ Main.py
@@ -54,22 +54,62 @@ class App:
     def __init__(self):
         pyxel.init(SIZE_X, SIZE_Y)
 
-        self.rect = []
+        self.rect = [
+            Rect(0, 0, SIZE_X, SIZE_Y)
+        ]
+
+        for i in range(0, 5):
+            self.split(random.randint(0, len(self.rect) - 1))
 
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        for rect in self.rect:
-            print(rect.get_sx(), rect.get_sy(), rect.get_w(), rect.get_h())
+        pass
+        #for rect in self.rect:
+        #    print(rect.get_sx(), rect.get_sy(), rect.get_w(), rect.get_h())
 
     def draw(self):
         pyxel.cls(0)
 
         for rect in self.rect:
-            pyxel.rect(rect.get_sx(), rect.get_sy(), rect.get_w(), rect.get_h(), 9)
+            pyxel.rectb(rect.get_sx(), rect.get_sy(), rect.get_w(), rect.get_h(), 9)
 
-    def generate(self):
-        pass
+    def split(self, rect):
+        print(rect)
+        _sx = self.rect[rect].get_sx()
+        _sy = self.rect[rect].get_sy()
+        _ex = self.rect[rect].get_ex()
+        _ey = self.rect[rect].get_ey()
+
+        is_split = False
+        x_or_y = random.randint(0, 1)
+
+        if x_or_y == 0: # x
+            a = _sx + 5
+            b = _ex - _sx - 5
+
+            if a < b:
+                print("X", a, b)
+                x = random.randint(a, b)
+                if x - _sx > 50 and _ex - x > 50:
+                    self.rect.append(Rect(_sx, _sy, x, _ey))
+                    self.rect.append(Rect(x, _sy, _ex, _ey))
+                    is_split = True
+
+        elif x_or_y == 1: # y
+            a = _sy + 5
+            b = _ey - _sy - 5
+
+            if a < b:
+                print("Y", a, b)
+                y = random.randint(a, b)
+                if y - _sy > 50 and _ey - y > 50:
+                    self.rect.append(Rect(_sx, _sy, _ex, y))
+                    self.rect.append(Rect(_sx, y, _ex, _ey))
+                    is_split = True
+        
+        if is_split:
+            self.rect.remove(self.rect[rect])
 
 if __name__ == "__main__":
     App()
