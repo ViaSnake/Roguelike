6b77bc88cc51f3ed1777e5990035ecda8707b5b0
snake <admin@viasnake.com>
2021-09-11 23:38:09 +0900

    Ref

--- Main.py
+++ Main.py
@@ -54,12 +54,11 @@ class App:
     def __init__(self):
         pyxel.init(SIZE_X, SIZE_Y)
 
-        self.rect = [
+        self.rects = [
             Rect(0, 0, SIZE_X, SIZE_Y)
         ]
 
-        for i in range(0, 5):
-            self.split(random.randint(0, len(self.rect) - 1))
+        self.generate_dungeon()
 
         pyxel.run(self.update, self.draw)
 
@@ -74,42 +73,44 @@ class App:
         for rect in self.rect:
             pyxel.rectb(rect.get_sx(), rect.get_sy(), rect.get_w(), rect.get_h(), 9)
 
-    def split(self, rect):
-        print(rect)
-        _sx = self.rect[rect].get_sx()
-        _sy = self.rect[rect].get_sy()
-        _ex = self.rect[rect].get_ex()
-        _ey = self.rect[rect].get_ey()
+    def generate_dungeon(self):
+        for i in range(0, 5):
+            rect = random.randint(0, len(self.rects) - 1)
 
-        is_split = False
-        x_or_y = random.randint(0, 1)
+            _sx = self.rects[rect].get_sx()
+            _sy = self.rects[rect].get_sy()
+            _ex = self.rects[rect].get_ex()
+            _ey = self.rects[rect].get_ey()
 
-        if x_or_y == 0: # x
-            a = _sx + 5
-            b = _ex - _sx - 5
+            is_split = False
+            x_or_y = random.randint(0, 1)
 
-            if a < b:
-                print("X", a, b)
-                x = random.randint(a, b)
-                if x - _sx > 50 and _ex - x > 50:
-                    self.rect.append(Rect(_sx, _sy, x, _ey))
-                    self.rect.append(Rect(x, _sy, _ex, _ey))
-                    is_split = True
+            if x_or_y == 0: # x
+                a = _sx + 5
+                b = _ex - _sx - 5
 
-        elif x_or_y == 1: # y
-            a = _sy + 5
-            b = _ey - _sy - 5
+                if a < b:
+                    print("X", a, b)
+                    x = random.randint(a, b)
+                    if x - _sx > 50 and _ex - x > 50:
+                        self.rects.append(Rect(_sx, _sy, x, _ey))
+                        self.rects.append(Rect(x, _sy, _ex, _ey))
+                        is_split = True
+              
+            elif x_or_y == 1: # y
+                a = _sy + 5
+                b = _ey - _sy - 5
 
-            if a < b:
-                print("Y", a, b)
-                y = random.randint(a, b)
-                if y - _sy > 50 and _ey - y > 50:
-                    self.rect.append(Rect(_sx, _sy, _ex, y))
-                    self.rect.append(Rect(_sx, y, _ex, _ey))
-                    is_split = True
+                if a < b:
+                    print("Y", a, b)
+                    y = random.randint(a, b)
+                    if y - _sy > 50 and _ey - y > 50:
+                        self.rects.append(Rect(_sx, _sy, _ex, y))
+                        self.rects.append(Rect(_sx, y, _ex, _ey))
+                        is_split = True
         
-        if is_split:
-            self.rect.remove(self.rect[rect])
+            if is_split:
+                self.rects.remove(self.rects[rect])
 
 if __name__ == "__main__":
     App()
